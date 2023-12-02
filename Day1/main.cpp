9a02330d7c9b677f8c6096acd14a4cbed9b70bfb
Christian Wansart <cwansart@users.noreply.github.com>
2023-12-02 09:31:20 +0100

    Day1 Part2

--- Day1/main.cpp
+++ Day1/main.cpp
@@ -2,6 +2,40 @@
 #include <iostream>
 #include <fstream>
 #include <cctype>
+#include <vector>
+
+std::vector<std::string> digits = {
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine"
+};
+
+unsigned int digit(std::string line, unsigned int i)
+{
+    for (unsigned int j = 0; j < digits.size(); j++)
+    {
+        std::string digit = digits[j];
+        // check if written digits exceeds length of line
+        if (i + digit.length() > line.length())
+        {
+            continue;
+        }
+
+        std::string line_rest = line.substr(i, digit.length());
+        if (line_rest == digit)
+        {
+            return j;
+        }
+    }
+    return -1;
+}
 
 int main(int argc, char** argv)
 {
@@ -13,18 +47,33 @@ int main(int argc, char** argv)
     while (std::getline(in, line))
     {
         int d1 = -1, d2 = -1;
-        for (size_t i = 0; i < line.length(); i++)
+        for (unsigned int i = 0; i < line.length(); i++)
         {
             char c = line[i];
             if (std::isdigit(c) != 0)
             {
                 if (d1 == -1)
                 {
-                    d1 = c;
+                    d1 = c - '0';
                 }
                 else
                 {
-                    d2 = c;
+                    d2 = c - '0';
+                }
+            }
+            else
+            {
+                int d = digit(line, i);
+                if (d >= 0)
+                {
+                    if (d1 == -1)
+                    {
+                        d1 = d;
+                    }
+                    else
+                    {
+                        d2 = d;
+                    }
                 }
             }
         }
@@ -34,7 +83,10 @@ int main(int argc, char** argv)
             d2 = d1;
         }
 
-        result += 10 * (d1-'0') + (d2-'0');
+        if (d1 > -1)
+        {
+            result += 10 * d1 + d2;
+        }
     }
    
     std::cout << result << std::endl;
